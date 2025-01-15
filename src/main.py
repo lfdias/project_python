410cd1055fb7c9e5aff0a442dd08e521f9a8de26
LuisDias <l.dias@andersenlab.com>
2025-01-15 11:26:38 +0000

    Dev done

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from operations import add, subtract, multiply, divide
+
+if __name__ == "__main__":
+    value = add(2, 2)
+    print(f"\n Value: {value} \n")
---
+++ src/operations.py
@@ -0,0 +1,18 @@
+
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Returns the difference of two numbers."""
+    return a - b
+
+def multiply(a, b):
+    """Returns the product of two numbers."""
+    return a * b
+
+def divide(a, b):
+    """Returns the quotient of two numbers. Raises an error if b is 0."""
+    if b == 0:
+        raise ValueError("Division by zero is not allowed.")
+    return a / b
